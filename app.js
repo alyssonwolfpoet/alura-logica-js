14f6361a7374d978bd2788c367758af58144004b
Alysson <alyssos@hotmail.com>
2025-01-26 02:04:20 -0300

    aula_3

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 9;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número inteiro entre 1 e 30')
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute;
+let = tentativas = 1;
 
-// se chuta for igual ao número secreto
-if(numeroSecreto == chute){
-   alert(`Isso ai Voçê descobriu o número secreto ${numeroSecreto}`);
-}else{
-    alert('Você errou :(')
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número inteiro entre 1 e 10');
+    // se chuta for igual ao número secreto
+    if(numeroSecreto == chute){
+        alert(`Isso ai Voçê descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    } else {
+        if(chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        // tentativas = tentativas + 1;
+        tentativas++;
+    }
 }
+
