e0500836181b5d56fd32fbe9fcd8dd7f5cea6a0c
Alysson <alyssos@hotmail.com>
2025-01-25 23:57:01 -0300

    aula_1

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número inteiro entre 1 e 30')
+
+if(numeroSecreto == chute){
+    console.log('Isso ai Voçê descobriu o número secreto secreto(5)');
+}
